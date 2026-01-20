b8a191f7ebe1cb20e8a4ccdc59d70972f62f4cdd
wmjarosko <207730420+wmjarosko@users.noreply.github.com>
2026-01-20 12:21:40 -0500

    first commit

---
+++ inst_nMap.ps1
@@ -0,0 +1,5 @@
+# Install Nmap using Winget (Built-in on modern Windows 10/11)
+winget install Insecure.Nmap
+
+# Alternatively, if you use Chocolatey:
+# choco install nmap
---
+++ inst_nmap.sh
@@ -0,0 +1,8 @@
+# Debian/Ubuntu/Kali
+sudo apt update && sudo apt install nmap -y
+
+# RHEL/CentOS/Fedora
+#sudo dnf install nmap -y
+
+# Arch Linux
+#sudo pacman -S nmap
